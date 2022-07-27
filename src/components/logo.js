63a7947050cba589f213a257d90eeac8bd62b668
monirshimul <monirshimul@gmail.com>
2022-07-27 13:00:13 +0600

    first commit

--- src/components/logo.js
+++ src/components/logo.js
@@ -7,7 +7,8 @@ import LogoSvg from 'components/icons/logo';
 export default function Logo({ isSticky, footer, ...props }) {
   return (
     <Link path="/" sx={styles.logo} {...props}>
-      <LogoSvg />
+      {/* <LogoSvg /> */}
+      <h3><span style={{fontSize:"25px", color:"red"}}>Fox</span> Catcher</h3>
     </Link>
   );
 }
